ff4694d18d2134b9f08d11c5b7af1047b533a7a2
Ali Shahidi <alishahidi1376@gmail.com>
2023-08-02 12:31:31 +0330

    add Helpers and refactoring BubbleSort

--- src/main/java/org/alishahidi/sort/BubbleSort.java
+++ src/main/java/org/alishahidi/sort/BubbleSort.java
@@ -1,5 +1,7 @@
 package org.alishahidi.sort;
 
+import org.alishahidi.tools.Helpers;
+
 public class BubbleSort {
     // Overloaded method with a single argument (ascending order)
     public static void sort(int[] array) {
@@ -10,15 +12,10 @@ public class BubbleSort {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
                 if ((reverse && array[j] < array[j + 1]) || (!reverse && array[j] > array[j + 1])) {
-                    swap(array, j, j + 1);
+                    Helpers.swap(array, j, j + 1);
+
                 }
             }
         }
-    }
-
-    private static void swap(int[] array, int i, int j) {
-        int tmp = array[i];
-        array[i] = array[j];
-        array[j] = tmp;
     }
 }
---
+++ src/main/java/org/alishahidi/tools/Helpers.java
@@ -0,0 +1,9 @@
+package org.alishahidi.tools;
+
+public class Helpers {
+    public static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+}
